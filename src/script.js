671d1c0d2f5c097010df7b1bfc50d6f513c87566
Julien Le Mee <julemee@gmail.com>
2023-12-24 16:36:32 -0500

    load model on the scene

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import GUI from 'lil-gui'
 
 /**
@@ -13,6 +14,19 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Models
+ */
+const gltfLoader = new GLTFLoader()
+
+gltfLoader.load(
+  '/models/Duck/glTF/Duck.gltf',
+  (gltf) =>
+  {
+      scene.add(gltf.scene.children[0])
+  }
+)
 
 /**
  * Floor
